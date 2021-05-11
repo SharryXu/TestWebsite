ed6c2b3e64762d37dcf055aed40d8458e5934e8b
Sharry Xu <sharry.r.xu@gmail.com>
2021-05-11 17:17:12 +0800

    Made message more readable.

--- Controllers/HealthyController.cs
+++ Controllers/HealthyController.cs
@@ -15,9 +15,9 @@ namespace TestWebsite.Controllers
         public IActionResult Ping()
         {
             if (DateTime.Now.Minute % 5 != 0){
-                return Ok($"{System.Environment.MachineName}: Sounds great!");
+                return Ok($"{System.Environment.MachineName}: Sounds great!{System.Environment.NewLine}");
             } else {
-                return BadRequest($"{System.Environment.MachineName}: Sounds bad!");
+                return BadRequest($"{System.Environment.MachineName}: Sounds bad!{System.Environment.NewLine}");
             }
         }
     }
